5cfb6e1b9b72fa00eae9671bbaed33ba0fce1049
RVSN451 <kvg.huawei@gmail.com>
2021-08-13 17:32:31 +0300

    Revision 1. spaceTrim deleted

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,26 +8,25 @@ import java.util.stream.Collectors;
 public class App {
 
     /*Признаки написания кода в функциональном стиле:
-    * 1. Использование чистых функций spaceTrim & dictionary (данные функции детерменированы и не имеют побочных эффектов);
-    * 2. Использование лямбда выражений (dictionary), что подтвенрждает детерминированный характер данной функции;
-    * 3. Использование МАНАДЫ (dictionary);*/
+     * 1. Использование чистых функций spaceTrim & dictionary (данные функции детерменированы и не имеют побочных эффектов);
+     * 2. Использование лямбда выражений (dictionary), что подтвенрждает детерминированный характер данной функции;
+     * 3. Использование МАНАДЫ (dictionary);*/
 
 
-    public static String[] spaceTrim (String msg) {
-        return msg.split("\\s{1,}");
-    }
-
-    public static List<String> dictionary (String [] msgArray){
-        return Arrays.stream(msgArray)
+    public static List<String> dictionary(String msg) {
+        return Arrays.stream(msg.replaceAll(("\\b[,.!?:;/-]"), "").split("\\s{1,}"))
+                .map(String::toUpperCase)
                 .sorted()
+                .distinct()
                 .collect(Collectors.toList());
     }
 
-    public static void main (String[]args) {
-            String msg = "ты туземец   пришёл  моя земля я хотеть    тебя    есть";
+    public static void main(String[] args) {
+        String msg = "Ты туземец   пришёл  моя земля, я хотеть    кушать. \n" +
+                "Читай моя записка, если нет много еды вечером: я точно тебя кушать!";
         System.out.println("Записка туземца: '" + msg + "'.\n");
-            System.out.println("Порядок слов в словаре: " + dictionary(spaceTrim(msg)));
-        }
+        System.out.println("Порядок слов в словаре: " + dictionary(msg));
+    }
 }
 
 
